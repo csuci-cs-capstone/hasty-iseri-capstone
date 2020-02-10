e5b30039fbe4ac3dc68a2fdd9cfcd72e30ded015
Tyler Hasty <ty.hasty@protonmail.com>
2020-02-09 22:21:44 -0800

    Configuring and outlining project, including creating src directory which will contain all resources, scripts and scenes.

---
+++ src/Systems/EchoFeedbackSystem/EchoFeedbackSystem.gd
@@ -0,0 +1,20 @@
+extends Node
+
+# This node will serve as an observer to the objects detectable by the different echo types.
+# When an echo makes contact with an interactable (GameWorld resource, obstacle, etc), that entity
+# will signal this system to store it in a queue. When the echo- 
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
